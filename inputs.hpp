905ef7c646e649d40ae0ea6c96aace12c00ffd4c
Linah <linah160406@gmail.com>
2026-05-11 15:21:32 +0300

    added inputs

---
+++ inputs.hpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+struct worker {
+    int worker_id;
+    string worker_name;
+    string contract_type;
+    int regular_hours_per_day;
+    int max_ovwertime_hours_per_day;
+    int hourly_rate;
+    double effiency_rate;
+    bool skill_heavy_lifting;
+    bool skill_packing;
+    bool skill_quality_inspection;
+    bool skill_labeling;
+};
+
+struct orders {
+    int order_id;
+    string order_name;
+    string complexity;
+    int deadline_days_left;
+    int priority;
+    int total_item_quantity;
+    double estimated_total_hours;
+    bool required_heavy_lifting;
+    bool required_packing;
+    bool required_quality_inspection;
+    bool required_labeling;
+};
+
+
+struct orderItems{
+    int order_id;
+    string item_name;
+    int item_quantity;
+    double processing_hour_per_item;
+    bool required_heavy_lifting;
+    bool required_packing;
+    bool required_quality_inspection;
+    bool required_labeling;
+};
